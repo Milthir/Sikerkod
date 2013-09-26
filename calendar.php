b879c45d17d70965b6c9d17e24180ec0a8dd05de
Vizdák Milán Gábor <milus0921@gmail.com>
2013-09-26 13:43:57 +0200

    Try to fix

--- calendar.php
+++ calendar.php
@@ -57,7 +57,7 @@ $startday = $thismonth['wday'];
 for ($i=0; $i<($maxday+$startday); $i++) {
     if(($i % 7) == 0 ) echo "<tr>n";
     if($i < $startday) echo "<td></td>n";
-    else echo "<td align='center' valign='middle' height='20px'id=".$cYear.$cMonth.($i - $startday + 1).">". ($i - $startday + 1) . "</td>n";
+    else echo "<td align='center' valign='middle' height='20px' id=".$cYear.$cMonth.($i - $startday + 1).">". ($i - $startday + 1) . "</td>n";
     if(($i % 7) == 6 ) echo "</tr>n";
 }
 ?>
